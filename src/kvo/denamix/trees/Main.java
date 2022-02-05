99ebacb79c3267b46c1a7a884670bc567311d24d
Alexxey <AlexKv2000@mail.ru>
2022-02-05 11:42:22 +0300

    add tree search

--- src/kvo/denamix/trees/Main.java
+++ src/kvo/denamix/trees/Main.java
@@ -2,40 +2,38 @@ package kvo.denamix.trees;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52};
-        int[] arr = new int[array.length];
-        mark(array, 0, array.length - 1, 0, arr);
+        int[] array = new int[]{10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52};
+        int[] levels = new int[array.length];
         build(array);
     }
 
-    static int[] mark(int[] arr, int left, int right, int level, int[] levels) {
-        if (arr.length == 0 || arr.length == 1) {
-            return levels;
-        }
+    static void mark(int left, int right, int level, int[] levels) {
+/*        if (arr.length == 0 || arr.length == 1) {
+            return;
+        }*/
         int middle;
         if (left == right) {
             levels[left] = level;
-            return levels; // ?
+            return;
         }
 
         if (left > right) {
-            return levels;
+            return;
         } else {
             middle = (left + right) / 2;
         }
-
+//        middle = (left + right) / 2;
         levels[middle] = level;
-        mark(arr, left, middle - 1, level + 1, levels);
-        mark(arr, middle + 1, right, level + 1, levels);
+        mark(left, middle - 1, level + 1, levels);
+        mark(middle + 1, right, level + 1, levels);
 
-        return levels;
     }
 
     static void build(int[] arr) {
         int[] levels = new int[arr.length];
 
-        mark(arr, 0, arr.length - 1, 0, levels);
-        int maxLevel = maxLevel(levels) + 1;
+        mark(0, arr.length - 1, 0, levels);
+        int maxLevel = levels[levels.length - 1] + 1;
         for (int level = 0; level < maxLevel; level++) {
             for (int i = 0; i < arr.length; i++) {
                 if (levels[i] == level) {
@@ -46,15 +44,5 @@ public class Main {
             }
             System.out.println();
         }
-    }
-
-    private static int maxLevel(int[] arr) {
-        int max = arr[0];
-        for (int i = 1; i < (int) arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
     }
 }
